afaf0c2ede4b9423b429f2445fab3abf30a6e24a
avishekprasadbarnwal <49061068+avishekprasadbarnwal@users.noreply.github.com>
2023-12-17 07:00:44 +0530

    01-CoreReact-JS

---
+++ App.js
@@ -0,0 +1,4 @@
+let createdelement = React.createElement("h1", {},"Hello World this is core React");
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(createdelement);
