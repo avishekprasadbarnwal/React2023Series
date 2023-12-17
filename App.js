2d983f55812697a7df6680e0cfd721de25df571e
avishekprasadbarnwal <49061068+avishekprasadbarnwal@users.noreply.github.com>
2023-12-17 14:13:10 +0530

    working of React behind the Scenes

--- App.js
+++ App.js
@@ -1,4 +1,43 @@
-let createdelement = React.createElement("h1", {},"Hello World this is core React");
+{
+    /* 
+        <div class="parent">
+            <div class="child">
+                <h1 class="sibling1">Sibling 1 text</h1>
+                <h2 class="sibling2">Sibling 2 text</h2>
+            </div>
+        </div> 
+    */
+}
+
+let createdelement = React.createElement(
+    "div", 
+    {
+        id: "parent",
+        data_core_react: "core react element"
+    },
+    React.createElement(
+        "div", 
+        {
+            id: "child"
+        },
+        [
+            React.createElement(
+                "h1", 
+                {
+                    id: "sibling1"
+                },
+                "Sibling 1"
+            ),
+            React.createElement(
+                "h2", 
+                {
+                    id: "sibling2"
+                },
+                "Sibling 2"
+            )
+        ]
+    )
+);
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(createdelement);
